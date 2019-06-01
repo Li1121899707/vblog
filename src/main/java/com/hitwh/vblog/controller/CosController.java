609d6b3a17e5e9fb6516823b283d98afd60d919e
liysuzy <1121899707@msn.cn>
2019-06-01 22:53:02 +0800

    添加MD5

--- src/main/java/com/hitwh/vblog/controller/CosController.java
+++ src/main/java/com/hitwh/vblog/controller/CosController.java
@@ -3,6 +3,7 @@ package com.hitwh.vblog.controller;
 import com.hitwh.vblog.response.BusinessException;
 import com.hitwh.vblog.response.CommonReturnType;
 import com.hitwh.vblog.response.EnumError;
+import com.hitwh.vblog.util.MyMd5;
 import com.hitwh.vblog.util.QCloudUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,12 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 
 @RestController
-@RequestMapping("/upload")
 public class CosController {
 
     @Autowired
     QCloudUtil qCloudUtil;
 
-    @RequestMapping("/image")
+    @RequestMapping("/upload_file")
     public CommonReturnType uploadImage(@RequestParam("image") MultipartFile multfile)throws Exception{
         if(multfile == null){
             throw new BusinessException(EnumError.PARAMETER_VALIDATION_ERROR);
@@ -28,13 +28,14 @@ public class CosController {
         String fileName = multfile.getOriginalFilename();
         // 获取文件后缀
         String prefix=fileName.substring(fileName.lastIndexOf("."));
+        String virtualFileName = MyMd5.md5(fileName + System.currentTimeMillis()) +  prefix;
         // 用uuid作为文件名，防止生成的临时文件重复
         final File tempFile = File.createTempFile("imagesFile-"+System.currentTimeMillis(), prefix);
         // 将MultipartFile转为File
         multfile.transferTo(tempFile);
 
         //调用腾讯云工具上传文件
-        CommonReturnType result = qCloudUtil.uploadFile(tempFile,"image/" + fileName);
+        CommonReturnType result = qCloudUtil.uploadFile(tempFile,"image/" + virtualFileName);
         //程序结束时，删除临时文件
         deleteFile(tempFile);
 
